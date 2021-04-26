df7b089a86a8b9cbcc4ceb644d0a050ea28c4c11
Your Name <you@example.com>
2021-04-26 14:01:54 +0000

    Initial commit

---
+++ area_perimeter_circle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Austin de Mora
+# Created on: April 2021
+# This program calculates the area of a circle
+#    with a radius of 
+
+import math
+
+
+def main():
+    # this function calculates the area
+
+    print("If the circle has a radius of 15 mm: ")
+    print("")
+    print("Area is {} mm².".format(math.pi * 15 ** 2))
+    print("Perimeter is {} mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
